dc2cf86a17a8eece9a8e7649330d03e7c97efd26
PrasunShrivastav <shrivastavprasun36@gmail.com>
2025-06-23 21:57:06 +0530

    basic db

---
+++ html+css+js+mongo/todoDB/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "s3cret";
+
+function auth(req, res, next) {
+  const token = req.headers.authorization;
+
+  try {
+    const response = jwt.verify(token, JWT_SECRET);
+
+    req.userId = response.id;
+    next();
+  } catch {
+    res.status(403).json({
+      message: "Incorrect creds",
+    });
+  }
+}
+
+module.exports = {
+  auth,
+  JWT_SECRET,
+};
